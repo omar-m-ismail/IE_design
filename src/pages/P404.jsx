171afbfa7c5cd04acf1fcc10e41632ac039ee062
omar mahmoud ismail <omar.mi196@hotmail.com>
2026-02-25 02:49:33 +0200

    Updates

--- src/pages/P404.jsx
+++
@@ -1,8 +0,0 @@
-function P404() {
-  return (
-    <div className="h-full flex justify-center items-center text-gray-700 bg-gray-100 font-bold">
-      404 | NOT FOUND
-    </div>
-  );
-}
-export default P404
